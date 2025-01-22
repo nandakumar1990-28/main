03b8a50dc755062883dd751e0023ab5827a97f74
nanda kumar <nandakumar.dj1990@gmail.com>
2025-01-22 17:33:05 +0530

    first commit

---
+++ src/main/java/com/microservices/hello_world/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.microservices.hello_world;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	
+	@GetMapping("/hello-world")
+	public String hello()
+	{
+		return "Hello from rest app..";
+	}
+}
